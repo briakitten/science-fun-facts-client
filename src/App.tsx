71390981f0c2420642bee72b1b2e663f058758c3
Bria <brialinza@gmail.com>
2022-08-19 17:08:24 -0600

    Added Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import axios from 'axios';
 // import logo from './logo.svg';
 import './App.css';
@@ -8,7 +9,7 @@ import FactList from './components/FactList';
 
 const URL = "https://science-fun-facts-api.herokuapp.com/api";
 
-function App() {
+function Home() {
   const [currentState, setCurrentState] = useState(0);
 
   const [data, setData] = useState<Array<FactData>>();
@@ -45,22 +46,52 @@ function App() {
   }
 
   return (
+    <div>
+      <p>
+        (This is the official version of this application. <a href="https://github.com/briakitten/ScienceFunFactsV1">V1 can be found here.</a>) <br/>
+        Made with Node.js, React, and PostgreSQL. <a href={URL}>Here is the backend</a>!
+      </p>
+      <div>
+        <button className="button1" onClick={clickRandomFact} style={{margin: 30, fontSize: 22}}>Query Random Fact</button>
+        <button className="button1" onClick={clickAllFacts} style={{margin: 30, fontSize: 22}}>Query All Facts</button>
+      </div>
+      {currentState === 1 && <Fact factData={randomFact}></Fact>}
+      {currentState === 2 && <FactList facts={data}></FactList>}
+    </div>
+  )
+}
+
+function RequestFact() {
+  return (
+    <div>Fact Page</div>
+  )
+}
+
+function App() {
+
+
+  return (
     <div className="App">
-      <header className="App-header">
-        <h1>Science Fun Facts API</h1>
-        <p>
-          (This is the official version of this application. <a href="https://github.com/briakitten/ScienceFunFactsV1">V1 can be found here.</a>) <br/>
-          Made with Node.js, React, and PostgreSQL. <a href={URL}>Here is the backend</a>!
-        </p>
-        <div style={{display: "flex", flexWrap: "wrap"}}>
-          <button className="button1" onClick={clickRandomFact} style={{margin: 30, fontSize: 22}}>Query Random Fact</button>
-          <button className="button1" onClick={clickAllFacts} style={{margin: 30, fontSize: 22}}>Query All Facts</button>
+      <BrowserRouter>
+        <header className="app-header">
+          <div className='tabs'>
+            <Link className="tab" to="/">Home</Link>
+            <Link className="tab" to="/requestfact">Request Fact</Link>
+          </div>
+          <h1>Science Fun Facts API</h1>
+          <br />
+        </header>
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/requestfact" element={<RequestFact />} />
+          </Routes>
         </div>
-        {currentState === 1 && <Fact factData={randomFact}></Fact>}
-        {currentState === 2 && <FactList facts={data}></FactList>}
-        <p>---------</p>
-        <a href="https://github.com/briakitten/science-fun-facts-client"><p>GitHub Source</p></a>
-      </header>
+        <footer className="app-footer">
+          <p>- - - - - - - - -</p>
+          <a href="https://github.com/briakitten/science-fun-facts-client"><p>GitHub Source</p></a>
+        </footer>
+      </BrowserRouter>
     </div>
   );
 }
